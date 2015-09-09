0f740084137731a4446c002133e25fe92b932809
peace098beat <peace098beat.t@gmail.com>
2015-09-09 22:19:34 +0900

    フォルダのドラッグドロップへ対応

--- AudioAnalyser01/gui_FileListWidget.py
+++ AudioAnalyser01/gui_FileListWidget.py
@@ -59,12 +59,7 @@ class MainWindow(QMainWindow):
 
 """
 
-import sys
 import time
-from PySide.QtCore import *
-from PySide.QtGui import *
-
-# -*- coding:utf-8 -*-
 import sys
 import os
 import warnings
@@ -76,9 +71,9 @@ from PySide.QtGui import *
 
 class CustomListModel(QAbstractListModel):
     """ データを格納するモデルクラス
-    リストに表示するデータ名だけでなく、その他の情報も持っていられる。
-    返却時に指定されたデータを渡す
-    """
+        リストに表示するデータ名だけでなく、その他の情報も持っていられる。
+        返却時に指定されたデータを渡す
+        """
 
     def __init__(self, parent=None, data=[]):
         super(CustomListModel, self).__init__(parent)
@@ -148,14 +143,15 @@ class CustomListModel(QAbstractListModel):
         else:
             return None
 
+# =============================================================================
 
 class CustomListDelegate(QStyledItemDelegate):
     """オリジナルのデリゲータ
     Listの表示を任意にできるが、Delegateを使うとデフォルトのカラー等は反映されなくなる
     """
 
-    THUMB_WIDTH = 30
-    MARGIN = 5
+    THUMB_WIDTH = 20
+    MARGIN = 1
 
     def __init__(self, parent=None):
         super(CustomListDelegate, self).__init__(parent)
@@ -165,6 +161,8 @@ class CustomListDelegate(QStyledItemDelegate):
 
         # imgファイルの存在場所を指定
         CURRENT_PATH = os.curdir
+        # Thumbnailを使う場合のフラグ
+        USE_THUMNAIL = False
         thumbName = 'ball2.png'
         BG_COLOR = [150, 150, 150]
         TXT_COLOR = [100, 10, 100]
@@ -181,22 +179,22 @@ class CustomListDelegate(QStyledItemDelegate):
             painter.setBrush(bgBrush)
             painter.drawRect(option.rect)
 
-
         # indexからデータを取り出す
         name = index.data(Qt.DisplayRole)
 
         # サムネイル画像の表示
         # ========================
-        imgpath = os.path.join(CURRENT_PATH, "images", thumbName)
-        # ファイルの存在確認
-        if not os.path.exists(imgpath):
-            print('File Not exist')
-            warnings.warn('file not exist')
+        if USE_THUMNAIL:
+            imgpath = os.path.join(CURRENT_PATH, "images", thumbName)
+            # ファイルの存在確認
+            if not os.path.exists(imgpath):
+                print('File Not exist')
+                warnings.warn('file not exist')
 
-        thumbImage = QPixmap(imgpath).scaled(self.THUMB_WIDTH, self.THUMB_WIDTH)
-        # 画像の表示場所を指定
-        r = QRect(option.rect.left(), option.rect.top(), self.THUMB_WIDTH, self.THUMB_WIDTH)
-        painter.drawPixmap(r, thumbImage)
+            thumbImage = QPixmap(imgpath).scaled(self.THUMB_WIDTH, self.THUMB_WIDTH)
+            # 画像の表示場所を指定
+            r = QRect(option.rect.left(), option.rect.top(), self.THUMB_WIDTH, self.THUMB_WIDTH)
+            painter.drawPixmap(r, thumbImage)
 
         # 文字色の変更
         # ==============
@@ -204,11 +202,17 @@ class CustomListDelegate(QStyledItemDelegate):
         # ペンを持たせる
         pen = QPen(tcolor, 0.5, Qt.SolidLine)
         painter.setPen(pen)
+
         # テキストを描く
-        r = QRect(option.rect.left() + self.THUMB_WIDTH + self.MARGIN,
-                  option.rect.top(),
-                  option.rect.width() - self.THUMB_WIDTH - self.MARGIN,
-                  option.rect.height())
+        if USE_THUMNAIL:
+            r = QRect(option.rect.left() + self.THUMB_WIDTH + self.MARGIN,
+                      option.rect.top(),
+                      option.rect.width() - self.THUMB_WIDTH - self.MARGIN,
+                      option.rect.height())
+        else:
+            r = option.rect
+
+        # テキストの表示
         painter.drawText(r,
                          Qt.AlignVCenter | Qt.AlignLeft,
                          "" + os.path.basename(name))
@@ -227,10 +231,9 @@ class MainWindow(QMainWindow):
     def __init__(self):
         QMainWindow.__init__(self)
         # self.setWindowIcon(QIcon('./FiSig/icon/icon_fifi.png'))
-
+        # D&Dを使う設定フラグ
         self.setAcceptDrops(True)
 
-        # ====================================
         # メンバ変数の定義
         # ====================================
         self.fileName = None
@@ -245,7 +248,6 @@ class MainWindow(QMainWindow):
         # 拡張子無しファイル名と、拡張子を返す
         self.name, self.ext = None, None
 
-        # ====================================
         # UIの生成
         # ====================================
         data = [
@@ -287,7 +289,6 @@ class MainWindow(QMainWindow):
         """ ファイルオープンダイアログの表示
         """
         fname, filt = QFileDialog.getOpenFileName(self, 'Open file', '/home', 'Wave Files (*.wav);; All Files (*)')
-        # print 'openFileDialog loadfile', str(fname)
 
         # 呼び出したファイルの格納
         self.cahangeFilePath(fname)
@@ -310,18 +311,36 @@ class MainWindow(QMainWindow):
         """
         print 'dropEvent'
 
-        files = [unicode(u.toLocalFile()) for u in event.mimeData().urls()]
+        urls = [unicode(u.toLocalFile()) for u in event.mimeData().urls()]
 
         # リストを消去
         self.myListModel.clearItems()
-        for fname in files:
-            # f = os.path.basename(fname)
-            self.myListModel.addItem({u"name": fname})
 
+        import glob
 
-            # 呼び出したファイルを格納
-            # file = files[0]
-            # self.cahangeFilePath(fname)
+        # 各URLをリストに追加する
+        for url in urls:
+            # URLがフォルダかファイルかを判定
+            if os.path.isdir(url):
+                # パスを正規化
+                abspath = os.path.abspath(url)
+                print 'This is Dir : ', abspath
+                # フォルダ直下の.wavを検索
+                searchpath = os.path.join(abspath, '*.wav')
+                # ディレクトリ直下のファイルを呼び出す
+                files = [r for r in glob.glob(searchpath)]
+                # ファイルリストから一つずつリストに追加
+                for file in files:
+                    self.myListModel.addItem({u"name": file})
+            else:
+                # URLがファイルの場合、(.wav)だけをリストにつか
+                abspath = os.path.abspath(url)
+                # 拡張子を取得
+                name, ext = os.path.splitext(abspath)
+                # .wavの場合だけ追加
+                if ext == '.wav':
+                    self.myListModel.addItem({u"name": abspath})
+                print 'This is not Dir :', abspath
 
     ##############################################
     # cahangeFilePath
@@ -364,6 +383,7 @@ class MainWindow(QMainWindow):
         print "ext", type(self.ext), str(self.ext)
         print "Please help SimpleFileLoader"
         print "-----------------------------------------"
+
 
 # =============================================================================
 ## DEMO
